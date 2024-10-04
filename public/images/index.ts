c36b926c276592142bd4538a16df509e0d65f018
Emma-nuel-orji <eorji452@gmail.com>
2024-10-04 11:14:26 +0100

    first commit

---
+++ public/images/index.ts
@@ -0,0 +1,21 @@
+import bear from "./bear.png";
+import coin from "./coin.png";
+import highVoltage from "./high-voltage.png";
+import notcoin from "./notcoin.png";
+import rocket from "./rocket.png";
+import trophy from "./trophy.png";
+import unload from "./unload.mp4"
+import snails from "./snails.mp4"
+import daily from "./daily.png"
+
+export {
+    bear,
+    coin,
+    highVoltage,
+    notcoin,
+    rocket,
+    trophy,
+    snails,
+    unload,
+    daily
+}
