091fa2a38d1ab6556208647188121a3f654ae8e2
Emma-nuel-orji <eorji452@gmail.com>
2024-11-04 13:54:15 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ export default function Home() {
         </div>
 
         {/* Original section */}
-        <div className="mt-[-0rem] text-5xl font-bold flex items-center">
+        <div className="mt-[-1rem] text-5xl font-bold flex items-center">
           <img src="/images/shell.png" width={48} height={48} alt="Coin" />
           <span className="ml-2">{user.points.toLocaleString()}</span>
         </div>
--- app/referralsystem/page.tsx
+++ app/referralsystem/page.tsx
@@ -25,10 +25,12 @@ export default function Home() {
 
   return (
     <main className="refer">
+      <div className="task-container" style={{ width: '100%' }}>
       <Link href="/"><img src="/images/info/output-onlinepngtools (6).png" width={24} height={24} alt="back" /></Link>
-      <h1 className="h text-4xl font-bold mb-8">Invite friends!</h1>
+      <h1 className="h text-4xl font-bold mb-4">Invite friends!</h1>
       <p className="p">You and your friend will recieve bonuses</p>
       <ReferralSystem initData={initData} userId={userId} startParam={startParam} />
+      </div>
     </main>
   )
 }
