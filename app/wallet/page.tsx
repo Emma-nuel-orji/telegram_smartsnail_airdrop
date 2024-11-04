a71c6e01f0526d3bcdbfeeeca722ae4943fcc815
Emma-nuel-orji <eorji452@gmail.com>
2024-11-04 17:12:38 +0100

    first commit

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -70,6 +70,7 @@ export default function Home() {
     );
   }
 
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       {/* <h1 className="text-4xl font-bold mb-8">TON Connect Demo</h1> */}
