14b159b3d7f291395ae91af460852f49f158f9e9
Emma-nuel-orji <eorji452@gmail.com>
2024-11-04 16:14:32 +0100

    first commit

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -72,7 +72,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Link href="/"><img src="/images/info/output-onlinepngtools (6).png" width={24} height={24} alt="back" /></Link>
       {/* <h1 className="text-4xl font-bold mb-8">TON Connect Demo</h1> */}
       {tonWalletAddress ? (
         <div className="flex flex-col items-center">
