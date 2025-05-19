d1367dcedd53e1c4634616d22e3490f63409c228
Emma-nuel-orji <eorji452@gmail.com>
2025-05-20 00:38:45 +0100

    first commit

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,7 @@
  export interface Task {
   id: string; 
     description: string;
+    
     completed: boolean;
     section: "main" | "daily" | "partners";
     type: "daily" | "permanent" | "flexible"; 
