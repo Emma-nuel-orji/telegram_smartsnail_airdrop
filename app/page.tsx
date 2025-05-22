1c08fef58f7af3e917aba3e2893de9a1104209df
Emma-nuel-orji <eorji452@gmail.com>
2025-05-23 00:07:15 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -944,6 +944,54 @@ useEffect(() => {
     </Link>
   </div>
 
+  <div className="absolute top-4 right-4 z-20">
+              <Link href="/staking" passHref>
+                <button
+                  onClick={handleFightClubClick}
+                  className="glass-shimmer-button text-white font-semibold px-4 py-2 rounded-md shadow-md flex items-center space-x-2"
+                >
+        <img
+          src="/images/gym.png"
+          alt="Fight Club"
+          className="w-6 h-6" // Adjust size as needed
+        />
+      </button>
+    </Link>
+  </div>
+
+
+  <div className="absolute top-4 right-4 z-20">
+              <Link href="/staking" passHref>
+                <button
+                  onClick={handleFightClubClick}
+                  className="glass-shimmer-button text-white font-semibold px-4 py-2 rounded-md shadow-md flex items-center space-x-2"
+                >
+        <img
+          src="/images/shop.png"
+          alt="Fight Club"
+          className="w-6 h-6" // Adjust size as needed
+        />
+      </button>
+    </Link>
+  </div>
+
+          
+  <div className="absolute top-4 right-4 z-20">
+              <Link href="/staking" passHref>
+                <button
+                  onClick={handleFightClubClick}
+                  className="glass-shimmer-button text-white font-semibold px-4 py-2 rounded-md shadow-md flex items-center space-x-2"
+                >
+        <img
+          src="/images/dish.png"
+          alt="Fight Club"
+          className="w-6 h-6" // Adjust size as needed
+        />
+      </button>
+    </Link>
+  </div>
+
+
   {/* Video */}
   <video
     src="/images/snails.mp4"
