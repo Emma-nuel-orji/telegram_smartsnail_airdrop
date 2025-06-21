9387a2dac75cb923d7f678cb8113702d8122d509
Emma-nuel-orji <eorji452@gmail.com>
2025-06-21 12:58:03 +0100

    first commit

--- app/gym/GymSubscriptions.tsx
+++ app/gym/GymSubscriptions.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { WebApp } from "@twa-dev/sdk"; 
 
 import React, { useEffect, useState } from "react";
