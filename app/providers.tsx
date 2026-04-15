dc1e0b49e4bf2f7bc91c81aa06097d297764ab3a
Alex Chinonso Onwubu <alexandersagenonso@gmail.com>
2026-04-15 09:28:26 +0100

    first commit

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@
 
 import { ReactNode } from 'react';
 import { WalletProvider } from './context/walletContext';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 interface ProvidersProps {
   children: ReactNode;
@@ -13,12 +12,8 @@ export function Providers({ children }: ProvidersProps) {
   const manifestUrl = process.env.NEXT_PUBLIC_MANIFEST_URL || 'https://telegram-smartsnail-airdrop.vercel.app/tonconnect-manifest.json';
   
   return (
-    // 1. Wrap EVERYTHING in the official TON Provider
-    // <TonConnectUIProvider manifestUrl={manifestUrl}>
-      {/* 2. Then keep your custom WalletProvider inside it */}
-      <WalletProvider manifestUrl={manifestUrl}>
-        {children}
-      </WalletProvider>
-    // {/* </TonConnectUIProvider> */}
+    <WalletProvider manifestUrl={manifestUrl}>
+      {children}
+    </WalletProvider>
   );
 }
